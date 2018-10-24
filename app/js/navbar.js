90b3fccc59827256975d1d303d78bbad9ad3efe4
manideep519 <manideepyadav359@gmail.com>
2018-10-24 22:14:32 +0530

    Completed the about section.

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -1,6 +1,16 @@
+//To expand and close the menu bar
 $(document).ready(function(){
   $('.nav-bar__icon').click(function(){
     $('.nav-bar__icon').toggleClass('active');
     $('.nav-bar__menu-bar').toggleClass('active');
   })
+});
+
+
+// To close the nav bar after clicking on the links
+$(document).ready(function () {
+  $(".link-click").click(function() {
+  	$('.nav-bar__icon').removeClass('active');
+    $('.nav-bar__menu-bar').removeClass('active');
+   });
 });
